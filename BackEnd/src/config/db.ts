b187c5480b402193991f7482af0fff97ea974371
Felipe21 <felipejurado032604@gmail.com>
2026-09-01 13:57:40 -0500

    feat(backend): BackEnd corregido en Index.ts y PaaS de desarrollo funcional

---
+++ BackEnd/src/config/db.ts
@@ -0,0 +1,17 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Configuración del Pool de conexiones a PostgreSQL usando los datos del Docker Compose
+export const pool = new Pool({
+  host: process.env.DB_HOST || 'postgres_db',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+pool.on('connect', () => {
+  console.log('📦 Conectado exitosamente a la base de datos PostgreSQL');
+});

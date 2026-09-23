f906d62276b3357e275d8356d123c3b276448907
Felipe21 <felipejurado032604@gmail.com>
2026-09-23 11:35:03 -0500

    fix: corregir configuración de conexión a la base de datos y mejorar el docker-compose

--- BackEnd/src/config/db.ts
+++ BackEnd/src/config/db.ts
@@ -9,7 +9,7 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 5432,
+  port: Number(process.env.DB_PORT) || 5432,
 });
 
 pool.on('connect', () => {
